db3c934a57826fb5f05c3eef718a8a203e88c1ec
Lizzie Chan <lchan217@gmail.com>
2019-03-06 02:25:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,9 @@ def prime?(integer)
   array = (1..integer-1).to_a 
   array.each do |num|
     if integer/num != 0 
-      true 
+      return true 
+    else 
+      return false 
     end 
   end 
 end 
